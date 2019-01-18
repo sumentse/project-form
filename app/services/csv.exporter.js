b7af33caf5d0105d84cf2035f3d7a50bc2bace6c
Sumen Tse <sumentse@users.noreply.github.com>
2019-01-18 13:49:29 -0500

    return void if no items

--- app/services/csv.exporter.js
+++ app/services/csv.exporter.js
@@ -6,7 +6,11 @@ export default () => {
     return {      
         $get: /*@ngInject*/ () => {
             return {
-                export: (data, filename = '')=>{
+                export: (data = [], filename = '')=>{
+
+                    if(data.length === 0){
+                        return;
+                    }
 
                     const options = {
                         filename: filename ? filename : `data_${data.length}_${moment().format('MMDDYYYY')}`,
